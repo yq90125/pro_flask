bac394664cee80950b0814076e91c7059ce4f9c1
yq_msi_laptop <948143880@qq.com>
2020-03-19 16:03:53 +0800

    commit

--- App/__init__.py
+++ App/__init__.py
@@ -1,6 +1,12 @@
+#encoding:utf-8
 from flask import Flask
 from App.views import init_view
+from App.models import init_model
 def create_app():
     app = Flask(__name__)
+    #uri 数据库+驱动(py中有的数据库比如sqlite不需要写驱动)://用户名:密码@主机:端口/数据库名
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    init_model(app)
     init_view(app)
     return app
--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,6 +1,6 @@
+#encoding:utf-8
 from .first_blue import blue
 from .second_blue import second_blue
-from flask import render_template
 def init_view(app):
     app.register_blueprint(blue)
     app.register_blueprint(second_blue)
